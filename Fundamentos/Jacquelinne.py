6552b6a83576c65938d477b0a068d76b53a861dd
JackieGCortez <gc1496042021@unab.edu.sv>
2021-08-21 13:43:55 -0600

    Create Jacquelinne.py
    Laboratotio

---
+++ Fundamentos/Jacquelinne.py
@@ -0,0 +1,12 @@
+from _typeshed import IdentityFunction
+
+
+print("se levanta usted a las 7 am")
+
+x = "si"
+
+if x== "si"
+print("si llegas a tiempo a tu  destino")
+
+else:
+print("no llegaras a tiempo")
